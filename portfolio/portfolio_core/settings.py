d5d7bb91c11dd00ad27e526518ab825ddc1be217
gros-pataplouf <sceres@pm.me>
2023-08-13 00:14:20 +0200

    fix: static files dir
    Former-commit-id: 0732b1654824a6ef794c44264994b3136f41df32

--- portfolio/portfolio_core/settings.py
+++ portfolio/portfolio_core/settings.py
@@ -147,7 +147,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static/")]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
