0ced75ab21744e293d59499bb3cb8740adb28585
gros-pataplouf <sceres@pm.me>
2023-09-01 20:44:43 +0200

    fix: hide admin area in production
    Former-commit-id: 2bed32ef81abc62001af65ef053d8a3e98c4c6fe

--- portfolio/portfolio_core/urls.py
+++ portfolio/portfolio_core/urls.py
@@ -3,11 +3,14 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 urlpatterns = [
     path("", include("portfolio_app.urls")),
-    path("admin/", admin.site.urls),
+    path(os.getenv("ADMIN_AREA"), admin.site.urls),
    
 ]
 
