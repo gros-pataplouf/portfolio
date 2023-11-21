dd6801902c19d702329d4ce59a6ad4ffc91b8e8c
gros-pataplouf <sceres@pm.me>
2023-11-21 20:22:27 +0100

    refactor: remove unused code

--- portfolio/portfolio_app/templatetags/portfolio_extras.py
+++ portfolio/portfolio_app/templatetags/portfolio_extras.py
@@ -16,25 +16,7 @@ def anchor_color(path, args):
 
 @register.filter
 def parsed_queried_skills(query_dict, skill_id):
+    """this is to determine if the id of a skill is in the query string, to adapt styling dynamically"""
     if query_dict.get("skills"):
         return str(skill_id) in query_dict.get("skills")
     return False
-
-
-@register.filter()
-def parse_items(args):
-    """parses a string containing items separated by # into a list"""
-    arg_list = args.split("#")
-    clean_args = [arg.strip(" ") for arg in arg_list]
-    return (clean_args)
-
-
-@register.filter()
-def addstring(arg1, arg2):
-    """concatenate arg1 & arg2"""
-    return str(arg1) + str(arg2)
-
-@register.filter()
-def even(parentloop):
-    """returns False if parentloop number is odd and True if it is even"""
-    return parentloop["counter0"] % 2 == 0
